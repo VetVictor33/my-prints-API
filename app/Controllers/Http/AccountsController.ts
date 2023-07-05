811767e2a75a1ce1836f256112a2e8705cba2300
Victor Feliciano <victorjfeliciano@gmail.com>
2023-07-05 11:31:20 -0300

    add schema validation to accountcontroller

--- app/Controllers/Http/AccountsController.ts
+++ app/Controllers/Http/AccountsController.ts
@@ -1,12 +1,16 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Account from 'App/Models/Account'
 import Hash from '@ioc:Adonis/Core/Hash'
+import Schemas from 'App/Schemas/Schemas'
 
 export default class AccountsController {
   public async store ({ request, response }: HttpContextContract) {
-    const { username, email, password } = request.body()
+    const { password } = request.body()
+    let validatedAccount = await Schemas.validadeAccount(request)
     const encryptedPassword = await Hash.make(password)
-    await Account.create({ username, email, password: encryptedPassword })
+    validatedAccount = {...validatedAccount, password: encryptedPassword}
+
+    await Account.create(validatedAccount)
     response.status(201)
     return {
       message: 'Account created!',
